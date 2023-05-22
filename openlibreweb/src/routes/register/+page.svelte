ec066fc989cd65e1fdf935d6aee902943dcc5555
arunabhdas <arunabhdas@gmail.com>
2023-05-22 04:18:36 -0400

    Implemented login

---
+++ openlibreweb/src/routes/register/+page.svelte
@@ -0,0 +1,55 @@
+
+<div class="py-8 sm:py-12">
+	<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+	  <div class="relative isolate overflow-hidden bg-slate-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
+        <div class="flex min-h-full flex-col justify-center px-6 py-6 lg:px-8">
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+              <img class="mx-auto h-10 w-auto" src="https://avatars.githubusercontent.com/u/134084984?s=400&u=43f288966b0607fa02e3497ded1a7c3ae58be167&v=4" alt="OpenLibreAI">
+              <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Register </h2>
+            </div>
+          
+            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form class="space-y-6" action="#" method="POST">
+                <div>
+                  <label for="email" class="block text-sm font-medium leading-6 text-white">Email address</label>
+                  <div class="mt-2">
+                    <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-stone-500 sm:text-sm sm:leading-6">
+                  </div>
+                </div>
+          
+                <div>
+                  <div class="flex items-center justify-between">
+                    <label for="password" class="block text-sm font-medium leading-6 text-white">Password</label>
+                    <div class="text-sm">
+                      <a href="#" class="font-semibold text-slate-400 hover:text-stone-300">Forgot password?</a>
+                    </div>
+                  </div>
+                  <div class="mt-2">
+                    <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-stone-500 sm:text-sm sm:leading-6">
+                  </div>
+                </div>
+          
+                <div>
+                  <button type="submit" class="flex w-full justify-center rounded-md bg-slate-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-stone-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-stone-500">Sign Up</button>
+                </div>
+              </form>
+          
+              <p class="mt-10 text-center text-sm text-slate-400">
+                Not a member?
+                <a href="#" class="font-semibold leading-6 text-slate-400 hover:text-stone-300">Start a 14 day free trial</a>
+              </p>
+            </div>
+          </div>
+		<svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2" aria-hidden="true">
+		  <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7" />
+		  <defs>
+			<radialGradient id="759c1415-0410-454c-8f7c-9a820de03641" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(512 512) rotate(90) scale(512)">
+			  <stop stop-color="#7775D6" />
+			  <stop offset="1" stop-color="#E935C1" stop-opacity="0" />
+			</radialGradient>
+		  </defs>
+		</svg>
+	  </div>
+	</div>
+  </div>
+
