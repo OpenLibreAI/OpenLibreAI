100c74c2fc577cc8ef12d16588e8e6dca345f1de
arunabhdas <arunabhdas@gmail.com>
2023-05-23 13:38:36 -0400

    Updated copy

--- open-libre-ai-web/src/routes/+page.svelte
+++ open-libre-ai-web/src/routes/+page.svelte
@@ -7,7 +7,7 @@
 	<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
 	  <div class="relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
 		<h2 class="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">OpenLibreAI is currently in private beta</h2>
-		<p class="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">Please request mods for a beta account </p>
+		<p class="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">Please register to beta test and provide feedback</p>
 		<form class="mx-auto mt-10 flex max-w-md gap-x-4"></form>
 		  <label for="email-address" class="sr-only">Email address</label>
 		  <!-- <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6" placeholder="Enter your email">-->
