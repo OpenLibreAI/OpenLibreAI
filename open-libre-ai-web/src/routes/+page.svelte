6e973457290e3453c321a53a639686c0d7f58821
arunabhdas <arunabhdas@gmail.com>
2023-05-23 13:35:47 -0400

    Updated links to socials

--- open-libre-ai-web/src/routes/+page.svelte
+++ open-libre-ai-web/src/routes/+page.svelte
@@ -7,7 +7,7 @@
 	<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
 	  <div class="relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
 		<h2 class="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">OpenLibreAI is currently in private beta</h2>
-		<p class="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">Please request mods for a beta account at the official <a href="https://discord.gg/v29eP5bZ">/r/AppIdeas Subreddit Discord server</a></p>
+		<p class="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">Please request mods for a beta account </p>
 		<form class="mx-auto mt-10 flex max-w-md gap-x-4"></form>
 		  <label for="email-address" class="sr-only">Email address</label>
 		  <!-- <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6" placeholder="Enter your email">-->
@@ -24,10 +24,6 @@
 			</button>
 			<button type="submit" class="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
 				<a href="https://twitter.com/openlibreai" target="_blank">Twitter</a>
-			</button>
-			<button type="submit" class="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
-				<a href="https://discord.gg/v29eP5bZ" target="_blank">Discord</a>
-			</button>
 		</form>
 		<svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2" aria-hidden="true">
 		  <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7" />
